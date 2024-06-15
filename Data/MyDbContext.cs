5774b2f761c83cacada43dc7d4ca987e5c068720
LazarevicV <vukadinlazarevic@gmail.com>
2024-06-15 23:56:03 +0200

    Db context

---
+++ Data/MyDbContext.cs
@@ -0,0 +1,23 @@
+﻿using IspitniProjekatRVAS.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IspitniProjekatRVAS.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products{ get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Additional configuration here if needed
+        }
+    }
+}
