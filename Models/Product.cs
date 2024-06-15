6cdd82293c4755c43431587ca703e6a7cc95ba91
LazarevicV <vukadinlazarevic@gmail.com>
2024-06-15 23:56:09 +0200

    Models

---
+++ Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace IspitniProjekatRVAS.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int CategoryId { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace IspitniProjekatRVAS.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+
+        public string Role { get; set; }
+        public string PasswordHash { get; set; }
+        public string PasswordSalt { get; set; }
+    }
+}
