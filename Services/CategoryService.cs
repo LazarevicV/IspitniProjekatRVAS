3ac4e853bab0527c31f4d6051e91f026339cea36
LazarevicV <vukadinlazarevic@gmail.com>
2024-06-22 21:55:20 +0200

    services

--- Services/CategoryService.cs
+++ Services/CategoryService.cs
@@ -17,5 +17,40 @@ namespace IspitniProjekatRVAS.Services
         {
             return await _context.Categories.ToListAsync();
         }
+
+        public async Task<bool> DeleteCategoryAsync(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null)
+            {
+                return false;
+            }
+
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task CreateCategoryAsync(string title)
+        {
+            var category = new Category { Title = title };
+
+            _context.Categories.Add(category); // Assuming Categories DbSet exists in ApplicationDbContext
+            await _context.SaveChangesAsync();  
+        }
+
+        public async Task<bool> UpdateCategoryTitleAsync(int id, string title)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null)
+            {
+                return false;
+            }
+
+            category.Title = title;
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
     }
 }
--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -17,5 +17,35 @@ namespace IspitniProjekatRVAS.Services
         {
             return await _context.Products.ToListAsync();
         }
+
+        public async Task<bool> DeleteProductAsync(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return false;
+            }
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task CreateProductAsync(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Product> GetProductAsync(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+        public async Task UpdateProductAsync(Product product)
+        {
+            _context.Entry(product).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
     }
 }
