a137f1b2032421ede9d05edff950fb50168249b0
LazarevicV <vukadinlazarevic@gmail.com>
2024-06-26 18:43:55 +0200

    models

--- Models/Product.cs
+++ Models/Product.cs
@@ -6,9 +6,12 @@
         public string Title { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
-
         public string ImageUrl { get; set; }
 
+        // Foreign key
         public int CategoryId { get; set; }
+
+        // Navigation property
+        public Category Category { get; set; }
     }
 }
---
+++ Models/Review.cs
@@ -0,0 +1,17 @@
+﻿namespace IspitniProjekatRVAS.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public int Rating { get; set; }
+
+        // Foreign keys
+        public int ProductId { get; set; }
+        public int UserId { get; set; }
+
+        // Navigation properties
+        public Product Product { get; set; }
+        public User User { get; set; }
+    }
+}
